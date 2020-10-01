1632ffe10e159a48d7ddab803058f5c95dbd1673
Chris Vazquez <006854322@coyote.csusb.edu>
2020-09-30 17:58:34 -0700

    Chris downloads

---
+++ Assets/Brekable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brekable : MonoBehaviour
+{
+    private void OnCollisionExit(Collision collision)
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
