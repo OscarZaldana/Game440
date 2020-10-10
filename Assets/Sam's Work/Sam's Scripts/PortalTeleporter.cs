7d5a8256249910aba017437db7c62729956428c4
Sam <samagee100@gmail.com>
2020-10-09 17:38:58 -0700

    teleportD=

---
+++ Assets/Sam's Work/Sam's Scripts/PortalTeleporter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject receiver;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			player.transform.position = new Vector3(receiver.transform.position.x, receiver.transform.position.y, 0);
+			Debug.Log("hit");
+		}
+	}
+
+}
